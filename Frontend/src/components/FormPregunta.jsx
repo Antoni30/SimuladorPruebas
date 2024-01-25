9e21aedcd7932cbefae047e39469582f1ab4067a
Antoni30 <artoapanta3@espe.edu.ec>
2024-01-24 23:24:25 -0500

    update

--- Frontend/src/components/FormPregunta.jsx
+++ Frontend/src/components/FormPregunta.jsx
@@ -62,7 +62,7 @@ const FormPregunta = ({idPersona}) => {
           type="submit"
           className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:outline-none focus:shadow-outline-blue"
         >
-          Enviar
+          Agregar
         </button>
       </form>
     </div>
